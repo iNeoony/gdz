b611724d92ee35158e11728714f33fa44ea929d3
crinny <isfellop@gmail.com>
2020-01-30 19:15:19 +0300

    :rocket: Improved README and made a few changes to the main class

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# gdz
-<img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></img>
+<h1 align="center">gdz</h1>
+<p align="center">
+    Враппер API приложения <a href="https://play.google.com/store/apps/details?id=com.gdz_ru">ГДЗ: мой решебник</a>.
+    <br /><br />
+    <img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg">
+</p>
 
-Враппер API [приложения gdz.ru](https://play.google.com/store/apps/details?id=com.gdz_ru&hl=ru).
+## Установка
+```sh
+pip install https://github.com/crinny/gdz/archive/master.zip
+``` 
+
+## Пример
+```python
+from gdz import GDZ
+
+gdz = GDZ()
+
+for book in gdz.books:
+    if "Алгебра" in book.title:
+        print(book.title)
+        for entry in gdz.book_structure(book):
+            print(entry.title)
+            for task in entry.tasks:
+                print(" -> " + task.title, end=", ")
+            print()
+        print()
+```
--- gdz/gdz.py
+++ gdz/gdz.py
@@ -2,25 +2,30 @@ import requests
 
 from gdz.types.book import Book
 from gdz.types.class_ import Class
-from gdz.types.subject import Subject
 from gdz.types.structure_entry import StructureEntry
+from gdz.types.subject import Subject
 
 
 class GDZ:
     API_ENDPOINT = "https://gdz-ru.com"
 
     def __init__(self):
+        self.session = requests.Session()
         self.main_info = requests.get(self.API_ENDPOINT).json()
 
-        self.classes = [
-            Class(**external_data) for external_data in self.main_info["classes"]
-        ]
-        self.subjects = [
+    @property
+    def classes(self):
+        return [Class(**external_data) for external_data in self.main_info["classes"]]
+
+    @property
+    def subjects(self):
+        return [
             Subject(**external_data) for external_data in self.main_info["subjects"]
         ]
-        self.books = [
-            Book(**external_data) for external_data in self.main_info["books"]
-        ]
+
+    @property
+    def books(self):
+        return [Book(**external_data) for external_data in self.main_info["books"]]
 
     def book_structure(self, book: Book):
         structure = requests.get(self.API_ENDPOINT + book.url).json()["structure"]
